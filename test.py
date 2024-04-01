9c3b5e237b90993ba267985b0b98660ee61f21a2
ZZHbible <804459116@qq.com>
2024-04-01 17:54:56 +0800

    Add logic to save the song.

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
+import time
+
 import requests
 import json
+import os
+from requests import get as rget
 
 
 def test_generate_music():
@@ -9,7 +13,7 @@ def test_generate_music():
         "mv": "chirp-v3-0",
         "title": "Sunshine in your Pocket",
         "continue_clip_id": None,
-        "continue_at": None
+        "continue_at": None,
     }
 
     r = requests.post("http://127.0.0.1:8000/generate", data=json.dumps(data))
@@ -19,9 +23,7 @@ def test_generate_music():
 
 
 def test_generate_lyrics():
-    data = {
-        "prompt":  ""
-    }
+    data = {"prompt": ""}
 
     r = requests.post("http://127.0.0.1:8000/generate/lyrics/", data=json.dumps(data))
     print(r.text)
@@ -30,3 +32,34 @@ def test_generate_lyrics():
 def get_lyrics(lid):
     r = requests.get(f"http://127.0.0.1:8000/lyrics/{lid}")
     print(r.text)
+
+
+def get_info(aid):
+    response = requests.get(f"http://127.0.0.1:8000/feed/{aid}")
+
+    data = json.loads(response.text)[0]
+
+    return data["audio_url"], data["metadata"]
+
+
+def save_song(aid, output_path="output"):
+    start_time = time.time()
+    while True:
+        audio_url, metadata = get_info(aid)
+        if audio_url:
+            break
+        elif time.time() - start_time > 90:
+            raise TimeoutError("Failed to get audio_url within 90 seconds")
+        time.sleep(30)
+    response = rget(audio_url, allow_redirects=False, stream=True)
+    if response.status_code != 200:
+        raise Exception("Could not download song")
+    index = 0
+    while os.path.exists(os.path.join(output_path, f"suno_{index}.mp3")):
+        index += 1
+    path = os.path.join(output_path, f"suno_{index}.mp3")
+    with open(path, "wb") as output_file:
+        for chunk in response.iter_content(chunk_size=1024):
+            # If the chunk is not empty, write it to the file.
+            if chunk:
+                output_file.write(chunk)
