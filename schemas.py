80836fdf6cdd2fc7b7c54937afad64bda8bb2177
CoengFan <melonsoul@qq.com>
2024-04-03 15:43:22 +0800

    docs: add fields desc

--- schemas.py
+++ schemas.py
@@ -15,11 +15,19 @@ class Response(BaseModel):
 class GenerateBase(BaseModel):
     """Generate with Custom Mode"""
 
-    prompt: str
-    mv: str
-    title: str
-    tags: str
-    continue_at: Optional[str] = None
+    prompt: str = Field(..., description="lyrics")
+    mv: str = Field(
+        ...,
+        description="model version, default: chirp-v3-0",
+        examples=["chirp-v3-0"],
+    )
+    title: str = Field(..., description="song title")
+    tags: str = Field(..., description="style of music")
+    continue_at: Optional[str] = Field(
+        default=None,
+        description="continue a new clip from a previous song, format mm:ss",
+        examples=["01:23"],
+    )
     continue_clip_id: Optional[str] = None
 
 
